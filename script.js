564096ab0af3309cb7a7b478896cb5855d1ee42a
casandrabenpor <casandra211@gmail.com>
2024-04-27 15:35:05 +0200

    styles parraf

--- script.js
+++
@@ -1,9 +0,0 @@
-function showForm(formId) {
-    // Oculta todos los formularios
-    document.querySelectorAll('form').forEach(form => {
-        form.classList.add('d-none');
-    });
-
-    // Muestra el formulario correspondiente al ID dado
-    document.getElementById(formId).classList.remove('d-none');
-}
